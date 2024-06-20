da3490a771d7fdb4f432c803d8084f46520adfaa
Danny Arellano <arellanodanielkeith@gmail.com>
2024-06-19 23:26:32 -0700

    init

---
+++ lua/plugins/init.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+        "clangd", "clang-format", "gopls",
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier"
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+        "cpp",
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+}
