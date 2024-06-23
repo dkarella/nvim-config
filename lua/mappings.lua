c114ab4b11bae7f0247e002770b8fae815bbc21f
Danny Arellano <arellanodanielkeith@gmail.com>
2024-06-22 19:47:02 -0700

    updates

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,3 +10,10 @@ map("n", "|", "<cmd>split<cr>", { desc = "hsplit" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "quit" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("t", "<C-c>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+map("n", "<leader>v", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggleable vertical term" })
+
+map("n", "<leader>h", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal new horizontal term" })
